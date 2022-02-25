2ebe377a37b5d16c559fb42018ed2a90fdf0c23b
ED <ed.ho@thinkpower-info.com>
2022-02-26 03:00:15 +0900

    POST

--- README.md
+++ README.md
@@ -3,12 +3,14 @@
 Ref: https://realpython.com/fastapi-python-web-apis/
 
 
-run sever
+#### run sever
 
 ```Shell
 uvicorn main:app --reload
 ```
 
-Swagger
+#### Swagger
+http://127.0.0.1:8000/docs
 
+#### ReDoc
 http://127.0.0.1:8000/redoc
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI
 
+from Item import Item
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {
+        "message": "Hello World",
+        "Swagger": "http://127.0.0.1:8000/docs",
+        "redoc": "http://127.0.0.1:8000/redoc",
+    }
 
 
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
